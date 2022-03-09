9e8d9cd04f2238f6eed7af9a61dc071ef339d79a
Hamid Mosalla <Xellarix@gmail.com>
2022-03-09 15:24:54 +0100

    add file loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import MediaCard from "./Components/MediaCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <MediaCard />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,15 @@ module.exports = {
             {
                 test: /\.svg$/,
                 loader: 'svg-inline-loader'
-            }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+              },
         ],
     },
     output: {
