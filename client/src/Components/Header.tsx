32ebf8306fc2017cd64b629f92c83bada4ab16b2
Hamid Mosalla <xellarix@gmail.com>
2022-06-22 23:01:31 +0200

    add constants for Header.tsx

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import { NavLink } from "react-router-dom";
 import favicon from "../assets/curated-art-icon.png";
+import { APP_TITLE, APP_DESCRIPTION } from "../utils/constants";
 
 const Header = () => {
     const activeStyle = { color: "#F15B2A" };
     return (
         <>
             <Helmet htmlAttributes={{ lang: "en-US" || undefined }}>
-                <title>{"Curated Art"}</title>
-                <meta property="og:title" content="Curated Art" />
+                <title>{APP_TITLE}</title>
+                <meta name="description" content={APP_DESCRIPTION} />
+                <meta property="og:title" content={APP_TITLE} />
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
                 <link rel="shortcut icon" href={favicon} type="image/x-icon" />
             </Helmet>
             <nav>
