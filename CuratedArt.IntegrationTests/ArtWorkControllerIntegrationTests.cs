bae8fcdb18c6ced6e190abcffff20bd40d968319
Hamid Mosalla <Xellarix@gmail.com>
2022-07-25 12:09:01 +0200

    add integration tests

--- CuratedArt.IntegrationTests/ArtWorkControllerIntegrationTests.cs
+++ CuratedArt.IntegrationTests/ArtWorkControllerIntegrationTests.cs
@@ -1,5 +1,8 @@
 using System.Net;
+using System.Net.Http.Json;
 using CuratedArt.Data;
+using CuratedArt.Data.Models;
+using CuratedArt.Dtos;
 using CuratedArt.IntegrationTests.Setup;
 
 namespace CuratedArt.IntegrationTests
@@ -8,18 +11,89 @@ namespace CuratedArt.IntegrationTests
     {
         public ArtWorkControllerIntegrationTests(IntegrationTestFactory<Program, CuratedArtDbContext> factory) : base(factory) { }
 
-        [Fact]
-        public async Task TestHomePage()
+        public async Task AddArtWorksAsync()
         {
-            var context = CuratedArtDbContext;
+            var artWorks = new List<ArtWork>
+            {
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Movie},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Music},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Painting},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Painting},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Movie},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Music},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Painting},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Music},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Painting},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Music},
+                new ArtWork{ Id = Guid.NewGuid(), DateReleased = DateTime.Now , Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Movie},
+            };
+
+            await CuratedArtDbContext.AddRangeAsync(artWorks);
+            await CuratedArtDbContext.SaveChangesAsync();
+        }
+
+        public async Task<Guid> AddArtWorkAsync()
+        {
+            var artWork = new ArtWork { Id = Guid.NewGuid(), DateReleased = DateTime.Now, Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Movie };
+
+            await CuratedArtDbContext.AddAsync(artWork);
+            await CuratedArtDbContext.SaveChangesAsync();
+
+            return artWork.Id;
+        }
+
+        public ArtWorkDto CreateArtWorkDto()
+        {
+            return new ArtWorkDto { Id = Guid.NewGuid(), DateReleased = DateTime.Now, Name = "Artwork1", Desc = "desc for art work 1", Type = ArtWorkType.Movie };
+        }
+
+
+        [Fact]
+        public async Task GetAllArtWorks_WhenCalled_AlwaysReturnsAllArtWorks()
+        {
+            await AddArtWorksAsync();
 
             var client = Factory.CreateClient();
 
             // Arrange
-            var defaultPage = await client.GetAsync("/");
+            var artWorksReponse = await client.GetFromJsonAsync<IList<ArtWorkDto>>("/api/v1/artworks");
 
             // Assert
-            Assert.Equal(HttpStatusCode.OK, defaultPage.StatusCode);
+            Assert.NotNull(artWorksReponse);
+            Assert.True(artWorksReponse.Count == 11);
+            Assert.IsType<List<ArtWorkDto>>(artWorksReponse);
+        }
+
+        [Fact]
+        public async Task GetArtWorksById_WhenCalled_ReturnsTheCorrectArtWork()
+        {
+            var artWorkId = await AddArtWorkAsync();
+
+            var client = Factory.CreateClient();
+
+            // Arrange
+            var artWorksReponse = await client.GetFromJsonAsync<ArtWorkDto>($"/api/v1/artworks/{artWorkId}");
+
+            // Assert
+            Assert.NotNull(artWorksReponse);
+            Assert.IsType<ArtWorkDto>(artWorksReponse);
+            Assert.True(artWorkId == artWorksReponse.Id);
+        }
+
+        [Fact]
+        public async Task PostArtWorks_WhenCalled_ReturnsTheCorrectArtWork()
+        {
+            var artWorkDto = CreateArtWorkDto();
+
+            var client = Factory.CreateClient();
+
+            // Arrange
+            var artWorksReponse = await client.PostAsJsonAsync<ArtWorkDto>($"/api/v1/artworks", artWorkDto);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, artWorksReponse.StatusCode);
+            Assert.NotNull(artWorksReponse);
+            Assert.IsType<ArtWorkDto>(artWorksReponse);
         }
     }
 }
