3fb4baa49719928f381400175794c2cebc3a0942
Hamid Mosalla <Xellarix@gmail.com>
2022-09-07 17:36:47 +0200

    add unit test for curated art project

---
+++ CuratedArt.UnitTests/ArtWorkControllerTests.cs
@@ -0,0 +1,28 @@
+using CuratedArt.Controllers;
+using CuratedArt.Dtos;
+using CuratedArt.Services;
+using NSubstitute;
+
+namespace CuratedArt.UnitTests
+{
+    public class ArtWorkControllerTests
+    {
+        private IArtWorkService _artWorkServiceFake = Substitute.For<IArtWorkService>();
+        private ArtWorkController _sut;
+
+        public ArtWorkControllerTests()
+        {
+            _sut = new ArtWorkController(_artWorkServiceFake);
+        }
+
+        [Fact]
+        public async Task Get_WhenCalled_ReturnsTheCorrectType()
+        {
+            _artWorkServiceFake.GetArtWorks().Returns(new List<ArtWorkDto>());
+
+            var result = await _sut.Get();
+
+            Assert.IsType<List<ArtWorkDto>>(result.Value);
+        }
+    }
+}
