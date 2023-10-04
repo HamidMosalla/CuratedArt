5b394dd9663110d4723940e6a350de165dfe7c2c
Hamid Mosalla <hamid.mosalla@lexisnexis.fr>
2023-10-04 11:45:14 +0200

    fix unit tests

--- CuratedArt.UnitTests/ArtWorkControllerTests.cs
+++ CuratedArt.UnitTests/ArtWorkControllerTests.cs
@@ -1,6 +1,8 @@
 using CuratedArt.Controllers;
+using CuratedArt.Data.Models;
 using CuratedArt.Dtos;
 using CuratedArt.Services;
+using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 
 namespace CuratedArt.UnitTests
@@ -18,11 +20,23 @@ namespace CuratedArt.UnitTests
         [Fact]
         public async Task Get_WhenCalled_ReturnsTheCorrectType()
         {
-            _artWorkServiceFake.GetArtWorks().Returns(new List<ArtWorkDto> { });
+            _artWorkServiceFake.GetArtWorks().Returns(new List<ArtWorkDto>
+            {
+                new ArtWorkDto
+                {
+                    Id = Guid.NewGuid(),
+                     Name = "Painting #2",
+                     Type = ArtWorkType.Painting,
+                     DateReleased = DateTimeOffset.Now,
+                      Desc = "The second epic painting"
+                }
+            });
 
             var result = await _sut.Get();
 
-            Assert.IsType<List<ArtWorkDto>>(result.Value);
+            var artWorks = (result.Result as OkObjectResult)?.Value as List<ArtWorkDto>;
+
+            Assert.NotEmpty(artWorks);
         }
     }
 }
---
+++ CuratedArt.UnitTests/Extensions/ActionResultExtensions.cs
@@ -0,0 +1,13 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+
+//namespace CuratedArt.UnitTests.Extensions
+//{
+//    public static class ActionResultExtensions
+//    {
+//        public static async Task<T> GetValue<T>(this ActionResult<T> actionResult) where T : class
+//        {
+//            var finalResult = await actionResult.Result.ExecuteResultAsync();
+
+//        }
+//    }
+//}
