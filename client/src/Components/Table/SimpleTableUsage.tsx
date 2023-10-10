e9a9d96f1c7d0d6602b810de49bc3f150e5bd369
Hamid Mosalla <hamid.mosalla@lexisnexis.fr>
2023-10-10 16:35:10 +0200

    change things back to string sort parameter

--- client/src/Components/Table/SimpleTableUsage.tsx
+++ client/src/Components/Table/SimpleTableUsage.tsx
@@ -1,26 +1,15 @@
 import { Box } from "@mui/material";
 import SimpleTable from "./SimpleTable";
 import Columns from "./Columns";
-import useFetchUsers, { SortingInfo } from "./api/useFetchUsers";
-import { useState, useMemo } from "react";
+import useFetchUsers from "./api/useFetchUsers";
+import { useState } from "react";
 import { SortingState } from "@tanstack/react-table";
 
 const SimpleTableUsage = () => {
     const [sorting, setSorting] = useState<SortingState>([]);
-
-    const sortedInfo = sorting.map(
-        (s) =>
-            ({
-                columnName: s.id,
-                sortingDirection: s.desc ? "DESC" : "ASC",
-            }) as SortingInfo
-    );
-
-    const sortByParam = useMemo(() => {
-        return sortedInfo.length === 1 ? sortedInfo[0] : sortedInfo;
-    }, [sortedInfo]);
-
-    const { data: users, isLoading, isFetching, error } = useFetchUsers(sortByParam);
+    const { data: users, isLoading, isFetching, error } = useFetchUsers({
+        sortBy: sorting.map((s) => `${s.id}:${s.desc ? "DESC" : "ASC"}`).join(","),
+    });
 
     // Optional: Handle the error if you'd like.
     if (error) {
--- client/src/Components/Table/api/useFetchUsers.ts
+++ client/src/Components/Table/api/useFetchUsers.ts
@@ -1,12 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export interface SortingInfo {
-  columnName: string;
-  sortingDirection: string;
-}
-
-function useFetchUsers(sortingInfoArray: SortingInfo[] | SortingInfo ) {
+function useFetchUsers({ sortBy = '' } = {}) {
   const [data, setData] = useState([]);
   const [maxPageSize, setMaxPageSize] = useState(1);
   const [error, setError] = useState(null);
@@ -17,8 +12,8 @@ function useFetchUsers(sortingInfoArray: SortingInfo[] | SortingInfo ) {
     async function fetchData() {
       setIsFetching(true);
       try {
-        console.log("sortingInfoArray", sortingInfoArray);
-        const response = await axios.get(`https://gorest.co.in/public/v2/users?sort=${sortingInfoArray}`);
+        console.log("sortBy", sortBy);
+        const response = await axios.get(`https://gorest.co.in/public/v2/users?sort=${sortBy}`);
         setData(response.data);
         const pages = Number(response.headers["x-pagination-pages"]);
         setMaxPageSize(pages === 0 ? 1 : pages);
@@ -31,7 +26,7 @@ function useFetchUsers(sortingInfoArray: SortingInfo[] | SortingInfo ) {
     }
 
     fetchData();
-  }, [sortingInfoArray]); // Re-run the effect when sortBy changes
+  }, [sortBy]); // Re-run the effect when sortBy changes
 
   return { data, maxPageSize, error, isLoading, isFetching };
 }
