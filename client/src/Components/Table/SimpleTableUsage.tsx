76479be17f827110bfb3098bb58516f653dc6c9d
Hamid Mosalla <hamid.mosalla@lexisnexis.fr>
2023-10-10 16:46:58 +0200

    change sortingInfo to string for the hook to not cause infinite loop

--- client/src/Components/Table/SimpleTableUsage.tsx
+++ client/src/Components/Table/SimpleTableUsage.tsx
@@ -2,14 +2,23 @@ import { Box } from "@mui/material";
 import SimpleTable from "./SimpleTable";
 import Columns from "./Columns";
 import useFetchUsers from "./api/useFetchUsers";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { SortingState } from "@tanstack/react-table";
 
 const SimpleTableUsage = () => {
     const [sorting, setSorting] = useState<SortingState>([]);
-    const { data: users, isLoading, isFetching, error } = useFetchUsers({
-        sortBy: sorting.map((s) => `${s.id}:${s.desc ? "DESC" : "ASC"}`).join(","),
-    });
+
+    const sortingInfo = useMemo(() => {
+      const sortedInfo = sorting.map((s) => ({
+        columnName: s.id,
+        sortingDirection: s.desc ? "DESC" : "ASC"
+      }));
+  
+      // Convert the sorting info to a string representation
+      return JSON.stringify(sortedInfo);
+  }, [sorting]);
+
+    const { data: users, isLoading, isFetching, error } = useFetchUsers(sortingInfo);
 
     // Optional: Handle the error if you'd like.
     if (error) {
